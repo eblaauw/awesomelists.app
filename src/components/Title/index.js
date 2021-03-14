c5c3783074b01de5affa5b565cf00944db11bf3c
Edzo <edzo@edzo.dev>
2021-03-14 16:19:40 +0400

    Add title

---
+++ src/components/Title/index.js
@@ -0,0 +1,10 @@
+import * as React from "react"
+
+// markup
+const Title = (props) => {
+  return (
+   <h1>{props.title}</h1>
+  )
+}
+
+export default Title
---
+++ src/pages/places/index.js
@@ -0,0 +1,12 @@
+import * as React from "react"
+import Title from "../../components/Title"
+
+const PlacesOverview = () => {
+    return (
+        <>
+            <Title title="List of awesome places around the world" />
+        </>
+    )
+}
+
+export default PlacesOverview
