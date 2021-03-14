eb7d353df6f0bf220b0aa7f8e9cb6de7019361d5
Edzo <edzo@edzo.dev>
2021-03-14 16:15:41 +0400

    Do not use GA now

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,12 +4,12 @@ module.exports = {
   },
   plugins: [
     "gatsby-plugin-image",
-    {
-      resolve: "gatsby-plugin-google-analytics",
-      options: {
-        trackingId: "",
-      },
-    },
+    // {
+    //   resolve: "gatsby-plugin-google-analytics",
+    //   options: {
+    //     trackingId: "",
+    //   },
+    // },
     "gatsby-plugin-react-helmet",
     "gatsby-plugin-sitemap",
     "gatsby-plugin-sharp",
